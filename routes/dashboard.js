7e5e0a4d85ce7189a16649576cd8ec2c90c24d3f
ngotruongan001 <anngo270398@gmail.com>
2024-08-23 07:52:05 +0700

    Update UI Loign

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -25,7 +25,8 @@ router.delete('/delete', indexController.deleteQuery);
 router.post('/dashboard/create', indexController.postCreate);
 router.post('/dashboard/create-tool', indexController.postCreateApplication);
 router.post('/dashboard/create/:tool/sensor', indexController.postCreateSensor);
-router.post('/dashboard/create/:tool/:sensor/container', indexController.postCreateContainerInSensor);
+router.post('/dashboard/create/:tool/:sensor/container', indexController.postCreateCon..
+    tainerInSensor);
 router.post('/dashboard/create/:tool/:sensor/subcribe', indexController.postCreateDataSubcribe);
 
 module.exports = router;
